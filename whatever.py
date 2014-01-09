5826ca761f56920d4776eb3c2c4a9f4264a26871
Chris Adams <wave@chrisadams.me.uk>
2014-01-09 21:43:56 +0000

    ho

--- whatever.py
+++ whatever.py
@@ -6,8 +6,8 @@ from datetime import datetime
 
 # then fetch a repo from somewhere
 # ./
-PATH = "/Users/chrisadams/Code/python-dojo-2013-01-09"
-GIT_MAP = {}
+PATH = "/Users/chrisadams/Code/python-dojo-2013-01-09/styles"
+CURRENT_FILES = []
 
 def _walk(tree):
     for b in tree.blobs:
@@ -21,38 +21,40 @@ def current_contents(commit):
     return list(_walk(commit.tree))
 
 
-def repo2dict(head_commit):
+def repo2dict(head_commit, files):
     queue = deque([head_commit])
     latest = {}
+    commits = set()
+
     while queue:
         commit = queue.popleft()
-        print commit
+        if commit.binsha in commits:
+            continue
+        commits.add(commit.binsha)
         date = commit.committed_date
-        for diff in commit.diff():
-            if not diff.b_blob:
-                continue
 
-            p = diff.b_blob.path
+        files_modified = commit.stats.files.keys()
+
+        for p in files_modified:
             try:
                 c = latest[p]
             except KeyError:
                 latest[p] = date
-
             else:
                 if date > c:
                     latest[p] = date
 
+        if set(latest.keys()) == files:
+            break
 
         queue.extend(commit.parents)
     return latest
-
-
 
 if __name__ == '__main__':
     repo = Repo(PATH)
     master_head_commit = repo.head.reference.commit
     contents = set(current_contents(master_head_commit))
-    times = repo2dict(master_head_commit)
+    times = repo2dict(master_head_commit, contents)
     d = {k: datetime.fromtimestamp(v).isoformat(' ') for k, v in times.items() if k in contents}
     from pprint import pprint
     pprint(d)
